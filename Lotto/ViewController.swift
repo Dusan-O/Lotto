dce4ad551d74eb566009974ba3ead7382e42f6a3
Dusan-O <dusan.orescanin@gmail.com>
2022-08-09 13:40:35 +0200

    Initial Commit

---
+++ Lotto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lotto
+//
+//  Created by Dusan Orescanin on 09/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
