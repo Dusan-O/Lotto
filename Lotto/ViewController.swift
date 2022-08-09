d806e97186f2758e8ae88ec917fb8ade44e1459b
Dusan-O <dusan.orescanin@gmail.com>
2022-08-09 21:03:19 +0200

    IBOutlet & IBAction in ViewController

--- Lotto/ViewController.swift
+++ Lotto/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var playButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func playAction(_ sender: Any) {
+    }
+    
 }
 
