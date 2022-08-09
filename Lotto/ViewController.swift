a04ca300ca4d701459b33c5971e54d8985b36192
Dusan-O <dusan.orescanin@gmail.com>
2022-08-09 21:13:38 +0200

    Change bin Go corner radius , empty 5 number labels & refractor - Extract to Method - renamed method setupUI

--- Lotto/ViewController.swift
+++ Lotto/ViewController.swift
@@ -12,9 +12,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var playButton: UIButton!
     
+    fileprivate func setupUI() {
+        /// Change btn GO cornerRadius
+        playButton.layer.cornerRadius = 10
+        /// Empty the 5 labels
+        stackView.arrangedSubviews.forEach { item in
+            /// First recuperate the label
+            let label = item.subviews.first as? UILabel /// label is a subView of our item that's a view
+            label?.text = "" /// Label is an optionnal
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
     @IBAction func playAction(_ sender: Any) {
